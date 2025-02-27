465f80efe851c2174dabd5c29e51eb1fb80f0dcd
adriarodrisan <1900adria@gmail.com>
2025-02-27 16:21:38 +0100

    creacion de el documento php y enviar los datos a este documento de para poder enviar los datos a la base de datos

---
+++ Paginas_web/insertar_datos_registro.php
@@ -0,0 +1,9 @@
+<?php
+require_once ('conexion.php');
+$dni= trim($_POST['dni']);
+$nombre= trim($_POST['nombre']);
+$telefono= trim($_POST['telefono']);
+$correo= trim($_POST['correo']);
+$contrasena= trim($_POST['contrasena']);
+$confirmar_contrasena= trim($_POST['confirmar_contrasena']);
+?>
