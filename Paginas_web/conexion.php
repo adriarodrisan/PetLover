80dbf9380f00f96d41c679ac82b8bc46e60c767f
adriarodrisan <1900adria@gmail.com>
2025-02-27 15:34:07 +0100

    creacion del documento para conectar la pagina con la base de datos

---
+++ Paginas_web/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$servername = 'localhost'; // este sera el nombre de nuestro servidor
+$database = 'protectora'; // nombre bd
+$username = 'petlove'; // Usuario bd
+$password = 'mascota'; // pass base de datos
+// creamos conexion
+$conn = new mysqli($servername, $username, $password, $database);
+//verificamos la conexion
+if ($conn->connect_error){
+    die("Conexion fallida: ". $conn->connect_error);
+}
+echo "<script>console.log('Este es un mensaje desde PHP');</script>";
+?>
