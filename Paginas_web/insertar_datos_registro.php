4022032ce6955659d1c049d2bfd293418fdf13ca
adriarodrisan <1900adria@gmail.com>
2025-03-05 15:36:17 +0100

    Revisar que hay contenido en las variables que recibimos del formulario si hay alguna variable vacia se ejecuta el echo y sale del programa.

--- Paginas_web/insertar_datos_registro.php
+++ Paginas_web/insertar_datos_registro.php
@@ -6,4 +6,9 @@ $telefono= trim($_POST['telefono']);
 $correo= trim($_POST['correo']);
 $contrasena= trim($_POST['contrasena']);
 $confirmar_contrasena= trim($_POST['confirmar_contrasena']);
+//revisar que los campos no sean null
+if(empty($dni) || empty($nombre) || empty($telefono) || empty($correo) || empty($contrasena) || empty($confirmar_contrasena)){
+    echo "Por favor, no dejes ningun hueco vacio en el formulario";
+    exit;
+}
 ?>
