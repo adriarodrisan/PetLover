c26f77eda3635711230675a37599c5b6beafc906
adriarodrisan <1900adria@gmail.com>
2025-03-17 19:51:18 +0100

    con este se mostraran los errores por consola

--- Paginas_web/login.php
+++ Paginas_web/login.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
 session_start();
 $servername = 'localhost'; // este sera el nombre de nuestro servidor
 $database = 'protectora'; // nombre bd
