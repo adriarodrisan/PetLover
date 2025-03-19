b2c7c2c5f2079402f43ab35a9224aaf79e21fe64
adrià Rodriguez Sanchez <151919370+adriarodrisan@users.noreply.github.com>
2025-03-19 15:57:56 +0100

    Corregir errores en el codigo

--- Paginas_web/login.php
+++ Paginas_web/login.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $opciones = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"];
 try {
     $db = new PDO('mysql:host=localhost;dbname=protectora', 'petlove', 'mascota');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTON);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch (PDOException $e){
     echo 'Falló la conixion: ' . $e->getMessage(); 
 }
