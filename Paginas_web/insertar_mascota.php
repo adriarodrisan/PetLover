c10be255e5c15d376eda6d36042ef5a8376f9588
adrià Rodriguez Sanchez <151919370+adriarodrisan@users.noreply.github.com>
2025-03-20 16:35:44 +0100

    Creacion del filtro de los animales no pueden tener el mismo numero de chip

--- Paginas_web/insertar_mascota.php
+++ Paginas_web/insertar_mascota.php
@@ -17,9 +17,17 @@ $raza= trim($_POST['raza']);
 $fecha_nacimiento= trim($_POST['FechaNacimiento']);
 $sexo= trim($_POST['Sexo']);
 $peso= trim($_POST['Peso']);
+$estado= trim($_POST['Estado']);
 $imagen= trim($_POST['Imagen']);
 if(empty($chip)|| empty($nombre)|| empty($especie)|| empty($raza)|| empty($fecha_nacimiento)|| empty($sexo)|| empty($peso)|| empty($imagen)){
     echo "Por favor, no dejes ningun hueco vacio en el formulario";
+    exit;
+}
+//verificar que no hay dos chips iguales
+$datos_animales= $db->query("SELECT * FROM animal WHERE  Chip_ID= '$chip' ");
+$datos_animales-> execute();
+if ($datos_animales->rowCount()>0){
+    echo "Este animal ya esta registrado porfavor revise el numero de chip que puede estar equivocado.";
     exit;
 }
 ?>
