cfa44034ebb497325a6cae4404863e8d49115251
Mnavaa <Navamargareth@gmail.com>
2025-03-28 19:47:44 +0100

    rol de protectora

--- Paginas_web/navbar.php
+++ Paginas_web/navbar.php
@@ -5,19 +5,21 @@ session_start();
   <ul>
     <li><a href="Home_Petlover.php">Inicio</a></li>    
     
-    <?php if (!isset($_SESSION['rol'])): ?>
+    <?php if ($_SESSION['rol'] === null ): ?>
       <li><a href="Inicio_Sesion.php">Iniciar Sesión</a></li>
       <li><a href="registro.html">Registrarse</a></li>
 
       <?php elseif ($_SESSION['rol'] === 'adoptante'): ?>
-      <li><a href="insertar_masacotas.php">Ver Mascotas</a></li>
+      <li><a href="mostrar_masacota.php">Ver Mascotas</a></li>
       <li><a href="cerrar_sesion.php">Cerrar Sesión</a></li>
 
+      <?php elseif ($_SESSION['rol'] === 'protectora'): ?>
+      <li><a href="insertar_mascota.php">Publicar Mascota</a></li>
+      <li><a href="cerrar_sesion.php">Cerrar Sesión</a></li>
 
-
-
-
-
+      <?php endif; ?>
+  </ul>
+</nav>
 <nav>
             <a href="SobreNosotros.html">Sobre Nosotros</a>
             <a href="#Adopcion">Cómo Adoptar</a>
