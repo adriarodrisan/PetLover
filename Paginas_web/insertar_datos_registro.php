ad78d492374fbef923a2be062493004a7868413a
adrià Rodriguez Sanchez <151919370+adriarodrisan@users.noreply.github.com>
2025-04-01 11:35:08 +0200

    aran para hacer la revision con session pero ahora es una cookies

--- Paginas_web/insertar_datos_registro.php
+++ Paginas_web/insertar_datos_registro.php
@@ -77,7 +77,7 @@ $registrar_usuarios ->execute();
 if ($registrar_usuarios){
 //if (mysqli_query($conn, $registrar_usuarios)){ 
     setcookie("rol", "adoptante", time() + (86400 * 30), "/"); 
-echo $_SESSION['usuario'], $_SESSION['rol'] ,'<!DOCTYPE html>
+echo  ,'<!DOCTYPE html>
             <html lang="es">
                 <head>
                     <meta charset="UTF-8">
