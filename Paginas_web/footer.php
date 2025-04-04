db26d808fe9c023a2039299690a8755629adc9cc
Mnavaa <Navamargareth@gmail.com>
2025-04-04 11:30:09 +0200

    nuestras redes sociles

--- Paginas_web/footer.php
+++ Paginas_web/footer.php
@@ -3,4 +3,10 @@
 <p>© 2025 PetLover | Contacto: <a href="mailto:petlovercomtact@gmail.com">petlovercomtact@gmail.com</a></p>
 <p><a href="/sobre-nosotros">Sobre Nosotros</a> | <a href="/terminos-condiciones">Términos y Condiciones</a> | <a href="/politica-privacidad">Política de Privacidad</a></p>
 </div>
+<div>
+        <p>Síguenos en nuestras redes sociales:</p>
+        <a href="" target="">Facebook</a> | 
+        <a href="" target="">Instagram</a> | 
+        <a href="" target="">Twitter</a>
+    </div>
 </footer>
