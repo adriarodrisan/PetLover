fd6923299b9a6d13f63abddcddb5c215aeff8379
Mnavaa <Navamargareth@gmail.com>
2025-04-04 11:29:00 +0200

    agregando al footer el correo de contacto y direccionamiento

--- Paginas_web/footer.php
+++ Paginas_web/footer.php
@@ -1,3 +1,6 @@
 <footer class="pie-pagina">
-    © 2025 PetLover | Contacto: petlover@gmail.com
+<div>
+<p>© 2025 PetLover | Contacto: <a href="mailto:petlovercomtact@gmail.com">petlovercomtact@gmail.com</a></p>
+<p><a href="/sobre-nosotros">Sobre Nosotros</a> | <a href="/terminos-condiciones">Términos y Condiciones</a> | <a href="/politica-privacidad">Política de Privacidad</a></p>
+</div>
 </footer>
