20f5015f89672b57fb0b682c8bac1e496f7e8027
adriarodrisan <1900adria@gmail.com>
2025-04-07 20:22:34 +0200

    se muestra más o menos el navbar

--- Paginas_web/navbar.php
+++ Paginas_web/navbar.php
@@ -1,12 +1,12 @@
 <?php
 $rol = $_COOKIE['rol'] ?? null;
 ?>
-<header>
-<div class="logo-contenedor">
-  <img id="logo" src="/Imagenes_Animales/logo.png"/>
-</div>
-<nav class="menu-centro">
-  <ul>
+<header class="navbar">
+    <div class="logo-contenedor">
+        <img id="logo" src="/Imagenes_Animales/logo.png" alt="Logo PetLover">
+    </div>
+    <div class="menu-centro">
+    <ul>
     <li><a href="SobreNosotros.html">Sobre Nosotros</a></li>
     <li><a href="#Adopcion">Cómo Adoptar</a></li>
     <li><a href="#Ayudas">Ayudanos</a></li>
@@ -18,13 +18,13 @@ $rol = $_COOKIE['rol'] ?? null;
         <li><a href="insertar_mascota.php">Publicar Mascota</a></li>
       <?php endif; ?>
       </ul>
-      </nav>
-      <div class="btn-session">
-      <?php if ($rol):?>
-        <a href="cerrar_sesion.php">Cerrar Session</a>
-      <?php else: ?>
-        <a href="Inicio_Sesion.html">Registrate/iniciar sesión</a>
-    <?php endif; ?>
     </div>
-    </header>
-</nav>
+
+    <div class="btn-session">
+    <?php if ($rol):?>
+        <a href="cerrar_sesion.php" class="boton-autenticacion">Cerrar Session</a>
+      <?php else: ?>
+        <a href="Inicio_Sesion.html" class="boton-autenticacion">Iniciar Sesión</a>
+        <?php endif; ?>
+      </div>
+</header>
