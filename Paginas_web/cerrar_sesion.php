4d3e71e990a5a3412c40bbf5b16bd071275a93af
adriarodrisan <1900adria@gmail.com>
2025-04-30 15:47:24 +0200

    corriguiendo renvios

--- Paginas_web/cerrar_sesion.php
+++ Paginas_web/cerrar_sesion.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_COOKIE['rol'])) {
     setcookie('rol', '', time() - 3600, '/');
-    header("Location: /Paginas_web/Home_Petlover.php");
+    header("Location: ./Home_Petlover.php");
     exit;
 }    
 ?>
