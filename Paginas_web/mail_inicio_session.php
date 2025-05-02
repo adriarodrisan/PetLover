658036420618084470136583d2113cb1c7063745
adriarodrisan <1900adria@gmail.com>
2025-05-02 19:29:24 +0200

    arreglando errores y ordenando el documento

--- Paginas_web/mail_inicio_session.php
+++ Paginas_web/mail_inicio_session.php
@@ -2,10 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require 'vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 function enviarCorreosInicioSession($correo,$nombre){
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    require 'vendor/autoload.php';
+    
     $mail = new PHPMailer(true);
 try {
     $mail = new PhPMailer();
