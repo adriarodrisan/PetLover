9443882ec69fdfa3320fd4a9475613b271529c60
adriarodrisan <1900adria@gmail.com>
2025-05-08 15:50:46 +0200

    probando poniendo todo en el documento de phpmailer

--- Paginas_web/prueba_de_envio_de_mails.php
+++ Paginas_web/prueba_de_envio_de_mails.php
@@ -2,12 +2,24 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'vendor/autoload.php';
+use Dompdf\Dompdf\Dompdf;
+use Dompdf\Dompdf\Options;
+//Configuracion del DOMPDF
+$options = new Options();
+$options->set('isRemoteEnabled', true);
+$dompdf = new Dompdf($options);
+//Contenido del PDF
+$html = '<h1>Este es el PDF</h1> <p>Generado con dompdf</p>';
+$dompdf->loadHtml($html);
+$dompdf->setPaper('A4', 'landscape');
+$dompdf->render();
+$tempFile = tempnam(sys_get_temp_dir(), 'pdf'). '.pdf';
+file_put_contents($tempFile, $dompdf->output());
 $mail = new PHPMailer(true);
 try {
-    $mail = new PhPMailer();
     $mail->isSMTP();                                           
     $mail->Host       = 'smtp.gmail.com';                    
     $mail->SMTPAuth   = true;                                   
@@ -19,11 +31,12 @@ try {
     $mail->addAddress('arodriguez@institutmvm.cat', 'Adria');
 
     $mail->isHTML(true);                              
-    $mail->Subject = 'este es una prueba de envio de correos';
-    $mail->Body    = 'pruebas de envio';
-    $mail->addAttachment($output, 'aplication/pdf','Prueba_de_pdf.pdf',false);
+    $mail->Subject = 'este es una prueba de envio de correos con pdf';
+    $mail->Body    = 'pruebas de envio con pdf';
+    $mail->addAttachment('temp.pdf', 'documento.pdf');
     $mail->send();
-    echo 'Message has been sent';
+    echo 'Mensaje enviado';
+    unlink ($tempFile);
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
