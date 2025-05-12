b13d4e4612f148321359a851eaa099ca7ee36032
Mnavaa <Navamargareth@gmail.com>
2025-05-12 16:32:47 +0200

    body mas alerta del registro exitoso

--- Paginas_web/insertar_datos_registro.php
+++ Paginas_web/insertar_datos_registro.php
@@ -89,6 +89,7 @@ if ($registrar_usuarios){
                 <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                     <link rel="stylesheet" href="./Css_Paginas/insertar_datos_registro.css">
                     <title>Registro Exitoso</title>
                 </head>
                 <body class="cuerpo">
