37ff8f5564c454beba946a46f1aa118b49380532
Mnavaa <Navamargareth@gmail.com>
2025-05-15 15:43:20 +0200

    css para alertas del procesor de adopcion

--- Paginas_web/procesar_adopcion.php
+++ Paginas_web/procesar_adopcion.php
@@ -78,6 +78,7 @@ if ($id_adoptante->rowCount() < 1) {
                 <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="./Css_Paginas/alertas_adopcion.css">
                     <title>Error 454</title>
                 </head>
                 <body>
