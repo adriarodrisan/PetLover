315bf964ccd5485aac46cf5d06eb9e9169f9914e
Mnavaa <Navamargareth@gmail.com>
2025-05-15 15:48:48 +0200

    css listo

--- Paginas_web/procesar_adopcion.php
+++ Paginas_web/procesar_adopcion.php
@@ -120,6 +120,7 @@ if ($registrar_adopcion){
                 <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                     <link rel="stylesheet" href="./Css_Paginas/alertas_adopcion.css">
                     <title>Registro Exitoso!</title>
                 </head>
                 <body>
